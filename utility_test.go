b46d138022457a77f47e23c01654a3f92ecf325a
Dustin Oprea <myselfasunder@gmail.com>
2018-05-04 05:09:27 -0400

    utility: Added tests.

--- utility_test.go
+++ utility_test.go
@@ -2,7 +2,60 @@ package exif
 
 import (
     "testing"
+    "os"
+
+    "io/ioutil"
+
+    "github.com/dsoprea/go-logging"
 )
+
+func TestDumpBytes(t *testing.T) {
+    f, err := ioutil.TempFile(os.TempDir(), "utilitytest")
+    log.PanicIf(err)
+
+    defer os.Remove(f.Name())
+
+    originalStdout := os.Stdout
+    os.Stdout = f
+
+    DumpBytes([]byte { 0x11, 0x22 })
+
+    os.Stdout = originalStdout
+
+    _, err = f.Seek(0, 0)
+    log.PanicIf(err)
+
+    content, err := ioutil.ReadAll(f)
+    log.PanicIf(err)
+
+    if string(content) != "DUMP: 11 22 \n" {
+        t.Fatalf("content not correct: [%s]", string(content))
+    }
+}
+
+func TestDumpBytesClause(t *testing.T) {
+    f, err := ioutil.TempFile(os.TempDir(), "utilitytest")
+    log.PanicIf(err)
+
+    defer os.Remove(f.Name())
+
+    originalStdout := os.Stdout
+    os.Stdout = f
+
+    DumpBytesClause([]byte { 0x11, 0x22 })
+
+    os.Stdout = originalStdout
+
+    _, err = f.Seek(0, 0)
+    log.PanicIf(err)
+
+    content, err := ioutil.ReadAll(f)
+    log.PanicIf(err)
+
+    if string(content) != "DUMP: []byte { 0x11, 0x22 }\n" {
+        t.Fatalf("content not correct: [%s]", string(content))
+    }
+}
 
 func TestDumpBytesToString(t *testing.T) {
     s := DumpBytesToString([]byte { 0x12, 0x34, 0x56 })
